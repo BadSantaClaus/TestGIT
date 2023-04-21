506702168d304feaaec493f1a85b07dc04ae28ec
Roman Goryachev <deadmopo3333@gmail.com>
2023-04-22 00:22:54 +0300

    Изменяю тест 4

--- src/java/Main.java
+++ src/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("First test");
         System.out.println("Second test");
         System.out.println("Third test");
-        System.out.println("Forth test");
+        System.out.println("Forth test1");
     }
 }
