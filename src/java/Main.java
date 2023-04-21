41323f37d43e76820790c3ed412bc4f99e8ac8c7
Roman Goryachev <deadmopo3333@gmail.com>
2023-04-21 23:45:29 +0300

    Add third and forth test

--- src/java/Main.java
+++ src/java/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("First test");
-
         System.out.println("Second test");
+        System.out.println("Third test");
+        System.out.println("Forth test");
     }
 }
